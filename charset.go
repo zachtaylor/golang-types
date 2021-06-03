1fd5c4714bf64d5008fd61e41f8d59a75d28d37a
Zach Taylor <zach@ztaylor.me>
2021-06-03 15:46:10 -0500

    add charset values
    Signed-off-by: Zach Taylor <zach@ztaylor.me>

---
+++ charset.go
@@ -0,0 +1,20 @@
+package types
+
+const (
+	// CharsetAlpha is "abcdefghijklmnopqrstuvwxyz"
+	CharsetAlpha = "abcdefghijklmnopqrstuvwxyz"
+	// CharsetAlphaCapital is "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	CharsetAlphaCapital = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// CharsetAlphaCapitalNumeric is "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	CharsetAlphaCapitalNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	// CharsetAlphaNumeric is "abcdefghijklmnopqrstuvwxyz0123456789"
+	CharsetAlphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+	// CharsetCapital is "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	CharsetCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// CharsetCapitalNumeric is "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	CharsetCapitalNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	// CharsetNumeric is "0123456789"
+	CharsetNumeric = "0123456789"
+	// CharsetText is a charset that restricts backslash (\\) and non-space control characters
+	CharsetText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}|;:'\",<.>/? \t"
+)
